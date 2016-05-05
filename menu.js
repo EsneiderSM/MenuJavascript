4e1d0e4f095ccc30a4d29532312a7f7c82cc9f40
Esneider Serna <esneider.marulanda@gmail.com>
2016-05-05 11:12:21 -0500

    firstcommit

---
+++ menu.js
@@ -0,0 +1,26 @@
+// Dropdown Menu
+var dropdown = document.querySelectorAll('.dropdown');
+var dropdownArray = Array.prototype.slice.call(dropdown,0);
+dropdownArray.forEach(function(el){
+	var button = el.querySelector('a[data-toggle="dropdown"]'),
+			menu = el.querySelector('.dropdown-menu');
+
+	button.onclick = function(event) {
+		if(!menu.hasClass('show')) {
+			menu.classList.add('show');
+			menu.classList.remove('hide');
+			event.preventDefault();
+			console.log("xx")
+		}
+		else {
+			menu.classList.remove('show');
+			menu.classList.add('hide');
+			event.preventDefault();
+			console.log("yy")
+		}
+	};
+})
+
+Element.prototype.hasClass = function(className) {
+    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+};
